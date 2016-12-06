4c5e2a7b16ece434a70d2b546bfb25c48eb185a9
shigemk2 <i.am.shige@gmail.com>
2016-12-06 22:12:30 +0900

    Add case classes

--- src/main/scala/com/example/RoutingSlip.scala
+++ src/main/scala/com/example/RoutingSlip.scala
@@ -2,5 +2,15 @@ package com.example
 
 import akka.actor._
 
+case class CustomerInformation(val name: String, val federalTaxId: String)
+case class ContactInformation(val postalAddress: PostalAddress, val telephone: Telephone)
+case class PostalAddress(
+                        val address1: String, val address2: String,
+                        val city: String, val state: String, val zipCode: String
+                        )
+case class Telephone(val number: String)
+case class ServiceOption(val id: String, val description: String)
+case class RegistrationData(val customerInformation: CustomerInformation, val contactInformation: ContactInformation, val serviceOption: ServiceOption)
+case class ProcessStep(val name: String, val processor: ActorRef)
 object RoutingSlipDriver extends CompletableApp(4) {
 }
